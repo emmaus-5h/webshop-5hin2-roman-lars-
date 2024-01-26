03b1aa23b9437f80a0ba0273fd10d13cc95d0728
Roman2301 <115638511+Roman2301@users.noreply.github.com>
2024-01-26 13:09:16 +0000

    afbeeldingen en prijs

--- db/create.sql
+++ db/create.sql
@@ -26,12 +26,12 @@ CREATE TABLE  prijzen(
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('man city', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('psg', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('real madrid', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('barcelona', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('man united', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('liverpool', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('man city', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 100);
+insert into products (name, description, code, price) values ('psg', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 100);
+insert into products (name, description, code, price) values ('real madrid', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 100);
+insert into products (name, description, code, price) values ('barcelona', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 100);
+insert into products (name, description, code, price) values ('man united', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 100);
+insert into products (name, description, code, price) values ('liverpool', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 100);
 
 
 
