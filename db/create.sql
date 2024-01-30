48370159f59742a5fe30578417b9922e5b9abdeb
Lars4hf <93325257+Lars4hf@users.noreply.github.com>
2024-01-30 08:36:23 +0000

    bijna n:m klaar

--- db/create.sql
+++ db/create.sql
@@ -15,6 +15,12 @@ CREATE TABLE kleur (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   kleurNAAM VARCHAR(20)
 );
+
+CREATE TABLE soorteerOPkleur (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  kleur_id VARCHAR (20)
+  products_id VARCHAR(20)
+);
 --
 -- populate with data
 --
@@ -25,17 +31,18 @@ CREATE TABLE kleur (
 --
 
 insert into products (name, description, code, price, kwaliteit) values ('liverpool', 'liverpool thuis shirt 23/24.', '816905633-0', 100, 8/10);
-insert into products (name, description, code, price, kwaliteit) values ('barcalona', 'barcalona thuis shirt 23/24.', '077030122-3', 100, 8/10);
-insert into products (name, description, code, price, kwaliteit) values ('man united', 'manchester united thuis shirt 23/24.' ,'445924201-X', 100, 8/10);
-insert into products (name, description, code, price, kwaliteit) values ('psg', 'psg thuis shirt 23/24.', '693155505-7', 100, 8/10);
-insert into products (name, description, code, price, kwaliteit) values ('real madrid', 'real madrid thuis shirt 23/24.', '686928463-6', 100, 9/10);
-insert into products (name, description, code, price, kwaliteit) values ('bayern munchen', 'bayern munchen thuis shirt 23/24.', '492662523-7', 100, 8.5/10);
 insert into products (name, description, code, price, kwaliteit) values ('liverpool uit shirt', 'liverpool uit shirt 23/24.', '816905633-1', 100, 8/10);
+insert into products (name, description, code, price, kwaliteit) values ('barcalona', 'barcalona thuis shirt 23/24.', '077030122-3', 100, 8/10);
 insert into products (name, description, code, price, kwaliteit) values ('barcalona uit shirt', 'barcalona uit shirt 23/24.', '077030122-4', 100, 8/10);
+insert into products (name, description, code, price, kwaliteit) values ('man united', 'manchester united thuis shirt 23/24.' ,'445924201-X', 100, 8/10);
 insert into products (name, description, code, price, kwaliteit) values ('man united uit shirt', 'manchester united uit shirt 23/24.' ,'445924201-Y', 100, 8/10);
+insert into products (name, description, code, price, kwaliteit) values ('psg', 'psg thuis shirt 23/24.', '693155505-7', 100, 8/10);
 insert into products (name, description, code, price, kwaliteit) values ('psg uit shirt', 'psg uit shirt 23/24.', '693155505-8', 100, 8/10);
+insert into products (name, description, code, price, kwaliteit) values ('real madrid', 'real madrid thuis shirt 23/24.', '686928463-6', 100, 9/10);
 insert into products (name, description, code, price, kwaliteit) values ('real madrid uit shirt', 'real madrid uit shirt 23/24.', '686928463-7', 100, 9/10);
+insert into products (name, description, code, price, kwaliteit) values ('bayern munchen', 'bayern munchen thuis shirt 23/24.', '492662523-7', 100, 8.5/10);
 insert into products (name, description, code, price, kwaliteit) values ('bayern munchen uit shirt', 'bayern munchen uit shirt 23/24.', '492662523-8', 100, 8.5/10);
+
 
 
 insert into  kleur (kleurNAAM) values ('zwart');
@@ -44,4 +51,12 @@ insert into  kleur (kleurNAAM) values ('rood');
 insert into  kleur (kleurNAAM) values ('blauw');
 insert into  kleur (kleurNAAM) values ('donker blauw');
 insert into  kleur (kleurNAAM) values ('geel/goud');
+insert into  kleur (kleurNAAM) values ('groen');
+insert into  kleur (kleurNAAM) values ('donker groen');
+
+
+
+
+
+
 --n:m kleur shirtjes
