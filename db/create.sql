8595ab613ac1ea2950d9802486ae3ff0024cae3d
Lars4hf <93325257+Lars4hf@users.noreply.github.com>
2024-02-02 13:30:57 +0000

    nm klaar

--- db/create.sql
+++ db/create.sql
@@ -64,7 +64,8 @@ insert into  kleur (kleurNAAM) values ('donker blauw');
 insert into  kleur (kleurNAAM) values ('geel/goud');
 insert into  kleur (kleurNAAM) values ('groen');
 insert into  kleur (kleurNAAM) values ('donker groen');
-
+insert into  kleur (kleurNAAM) values ('grijs');
+insert into  kleur (kleurNAAM) values ('roze');
 
 
 insert into  soorteerOPkleur (products_id, kleur_id) values (1, 2);
@@ -80,20 +81,23 @@ insert into  soorteerOPkleur (products_id, kleur_id) values (3, 6);
 insert into  soorteerOPkleur (products_id, kleur_id) values (4, 2);
 insert into  soorteerOPkleur (products_id, kleur_id) values (4, 3);
 insert into  soorteerOPkleur (products_id, kleur_id) values (4, 4);
-insert into  soorteerOPkleur (products_id, kleur_id) values (1, 2);
-insert into  soorteerOPkleur (products_id, kleur_id) values (1, 3);
-insert into  soorteerOPkleur (products_id, kleur_id) values (2, 1);
-insert into  soorteerOPkleur (products_id, kleur_id) values (2, 2);
-insert into  soorteerOPkleur (products_id, kleur_id) values (2, 7);
-insert into  soorteerOPkleur (products_id, kleur_id) values (2, 8);
-insert into  soorteerOPkleur (products_id, kleur_id) values (3, 3);
-insert into  soorteerOPkleur (products_id, kleur_id) values (3, 4);
-insert into  soorteerOPkleur (products_id, kleur_id) values (3, 5);
-insert into  soorteerOPkleur (products_id, kleur_id) values (3, 6);
-insert into  soorteerOPkleur (products_id, kleur_id) values (4, 2);
-insert into  soorteerOPkleur (products_id, kleur_id) values (4, 3);
-insert into  soorteerOPkleur (products_id, kleur_id) values (4, 4);
-
+insert into  soorteerOPkleur (products_id, kleur_id) values (5, 1);
+insert into  soorteerOPkleur (products_id, kleur_id) values (5, 2);
+insert into  soorteerOPkleur (products_id, kleur_id) values (5, 3);
+insert into  soorteerOPkleur (products_id, kleur_id) values (6, 2);
+insert into  soorteerOPkleur (products_id, kleur_id) values (6, 8);
+insert into  soorteerOPkleur (products_id, kleur_id) values (7, 2);
+insert into  soorteerOPkleur (products_id, kleur_id) values (7, 3);
+insert into  soorteerOPkleur (products_id, kleur_id) values (7, 5);
+insert into  soorteerOPkleur (products_id, kleur_id) values (8, 1);
+insert into  soorteerOPkleur (products_id, kleur_id) values (8, 2);
+insert into  soorteerOPkleur (products_id, kleur_id) values (8, 3);
+insert into  soorteerOPkleur (products_id, kleur_id) values (8, 5);
+insert into  soorteerOPkleur (products_id, kleur_id) values (9, 2);
+insert into  soorteerOPkleur (products_id, kleur_id) values (9, 3);
+insert into  soorteerOPkleur (products_id, kleur_id) values (10, 2);
+insert into  soorteerOPkleur (products_id, kleur_id) values (10, 9);
+insert into  soorteerOPkleur (products_id, kleur_id) values (10, 10);
 --n:m kleur shirtjes
 
 
